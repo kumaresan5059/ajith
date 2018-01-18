a5fc40e10bbf0394be76183e46b8cd23a69408f8
ajithmurugan <35128572+ajithmurugan@users.noreply.github.com>
2018-01-18 22:18:35 +0530

    Create 1.c

---
+++ gd/1.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main()
+{
+ int a;
+ printf("enter the number\n");
+ scanf("%d",&a);
+ if(a<0)
+ printf("number is negative",a);
+ if(a==0)
+ printf("number is zero",a);
+ if(a>0)
+ printf("number is positive",a);
+ return 0;
+ }
