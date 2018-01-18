05f5827cd9b42345d5e0277d7ef2647d3b684044
ajithmurugan <35128572+ajithmurugan@users.noreply.github.com>
2018-01-18 22:20:21 +0530

    Create C2.c

---
+++ gd/C2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int v;
+printf("enter the number");
+scanf("%d",&v);
+if(v%2==0)
+{
+	printf("numberis even");
+}
+	else
+	{
+	printf("number is odd");
+  }
+return 0;
+} 
